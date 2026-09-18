5bd512c580834fcd19844051dcc770dc0bfedbbe
sherali <sheraliakbarov699@gmail.com>
2026-09-18 17:29:51 +0500

    Add task 6

---
+++ myenv/6 ta amaliy/6-amaliy.py
@@ -0,0 +1,10 @@
+my_list = [1, 2, 3, 4, 5]
+
+a = my_list
+b = [1, 2, 3, 4, 5]
+
+print("a is b:", a is b)
+print("a is not b:", a is not b)
+
+print("3 in my_list:", 3 in my_list)
+print("8 not in my_list:", 8 not in my_list)
